dde88d4aa652a41b216b2979824ee8ef2af7b579
Akriti Kapur <akritikapur1@gmail.com>
2017-11-12 06:42:11 -0700

    added skeleton structure for board

---
+++ board.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+class Board:
+    """
+        Defines the Board for Chain-Reaction
+    """
+
+    def __init__(self, width=5, height=5):
+        pass
+
+    def get_available_moves(self):
+        pass
+
+    def get_max_critical_mass_for_cell(self, i, j):
+        pass
+
+    def move(self):
+        pass
+
+    def place_mark(self):
+        pass
+
+    def chain(self):
+        pass
+
+    def reward(self):
+        pass
