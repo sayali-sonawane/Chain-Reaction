a3868c857ce181111d0f08c0cca42d50a057c2ba
Akriti Kapur <akritikapur1@gmail.com>
2017-12-08 09:49:37 -0700

    added basic structure for minimax agent

---
+++ minimax_agent.py
@@ -0,0 +1,17 @@
+"""
+    This module implements the Minimax algorithm for chain reaction.
+
+    :example: An instance of the class can be made in the following way:
+
+"""
+
+
+class MinimaxAgent:
+    def __init__(self):
+        pass
+
+    def get_move(self):
+        pass
+
+    def minimax(self):
+        pass
